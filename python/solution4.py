394a396a4a3691a0e0db58dad386d89c47d3eae1
Chris Young <chris@implicit.net>
2023-06-17 20:04:05 +1000

    solution4

---
+++ python/solution4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+def solution4(filename):
+    part1 = part2 = 0
+    with open(filename) as file:
+        for line in file:
+            line = line.rstrip()
+            left, right = line.split(",")
+
+            lefts = [int(x) for x in left.split('-')]
+            rights = [int(x) for x in right.split('-')]
+
+            if lefts[0] >= rights[0] and lefts[1] <= rights[1]:
+                part1 += 1
+                part2 += 1
+                continue
+            elif rights[0] >= lefts[0] and rights[1] <= lefts[1]:
+                part1 += 1
+                part2 += 1
+                continue
+            if rights[0] <= lefts[0] <= rights[1]:
+                part2 += 1
+                continue
+            if lefts[0] <= rights[0] <= lefts[1]:
+                part2 += 1
+                continue
+
+
+    return part1, part2
+
+
+if __name__ == '__main__':
+    print(solution4("../input/4.txt"))
